440c2895bf41aadf2fd8a5a4dde602f5d6e63c21
aasurjya <ahandique8@gmail.com>
2025-05-08 19:32:32 +0530

    Initial Commit

---
+++ PointCloud/PointCloudApp.swift
@@ -0,0 +1,17 @@
+//
+//  PointCloudApp.swift
+//  PointCloud
+//
+//  Created by ihub-devs on 08/05/25.
+//
+
+import SwiftUI
+
+@main
+struct PointCloudApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
